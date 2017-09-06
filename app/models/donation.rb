fbbd5b624e6608e7b4f6e72d4e0366441e23cf5d
YOUR NAME <pong_thailand@hotmail.com>
2017-09-06 14:13:46 +0700

    added required fields

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,6 +1,7 @@
 class Donation < ApplicationRecord
+  validates :email, :name, :tag, :presence => true
   belongs_to :project
   scope :approved, -> {where(:approval => true)}
   scope :pending, -> {where(:approval => false)}
-  
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+  validates :email, :address, :title, :presence => true
   has_many :donations, dependent: :destroy
   extend FriendlyId
   friendly_id :title, :use => :slugged
