ecf733df07b827061c15db43f3fafc3a18e1b44d
YOUR NAME <pong_thailand@hotmail.com>
2017-09-07 15:58:56 +0700

    testing with 3 social acc.

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus email linkedin wechat reddit hacker_news)
+  config.allow_sites = %w(twitter facebook google_plus)
 end
