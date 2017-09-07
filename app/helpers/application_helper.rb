cc60e03bb21ec26cc32e86e48c0fce2a13e5104f
YOUR NAME <pong_thailand@hotmail.com>
2017-09-07 16:14:56 +0700

    metatags changed dynamically with page each projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(project_title)
+  content_for(:title) { project_title }
+end
+
+def meta_description(project_text)
+  content_for(:meta_description) { project_text }
+end
 end
